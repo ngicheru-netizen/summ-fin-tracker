a21eebdf00625af9c301265cdfb7fa60bafa1e7f
ngicheru-netizen <n.gicheru@alustudent.com>
2026-06-14 18:52:01 +0300

    localStorage storage module added

---
+++ scripts/storage.js
@@ -0,0 +1,57 @@
+function loadTransactions() {
+  // input: nothing
+  // output: array of transactions (If no transactions, empty array)
+  // code logic: get data from localStorage, parse JSON and return an array
+  const getarray = localStorage.getItem("app:transactions");
+  if (getarray) {
+    const parsed = JSON.parse(getarray);
+    return parsed;
+  } else {
+    return []; //return array
+  }
+}
+
+function saveTransactions(transactionArray) {
+  // input: array of transactions (when addTransaction is clicked)
+  // Ouput: nothing returns or true, if function is successful
+  //code logic: save added transactions to localStorage
+  localStorage.setItem("app:transactions", JSON.stringify(transactionArray));
+}
+
+function loadSettings() {
+  //input: nothing
+  //ouptut: Object with settings from localStorage
+  // code logic: load settings from localStorage (object). Empty Object if none
+  const getsettings = localStorage.getItem("app:settings");
+  if (getsettings) {
+    const parsed = JSON.parse(getsettings);
+    return parsed;
+  } else {
+    return {}; //return object, not array([])
+  }
+}
+function saveSettings(settingsObject) {
+  //input: addition to object
+  //output: nothing
+  //code logic: Save added settings to localStorage
+  localStorage.setItem("app:settings", JSON.stringify(settingsObject));
+}
+function loadCategories() {
+  //input: nothing
+  //output: array of categories (if none, empty array)
+  //logic: load object with categories from localStorage.
+  const getCategories = localStorage.getItem("app:categories");
+  if (getCategories) {
+    const parsed = JSON.parse(getCategories);
+    return parsed;
+  } else {
+    return [];
+  }
+}
+
+function saveCategories(categoriesObject) {
+  //input: add full array of categories
+  //output: nothing
+  //logic: save added categories
+  localStorage.setItem("app:categories", JSON.stringify(categoriesObject));
+}
